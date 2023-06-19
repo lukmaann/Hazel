42031b6d439041617a25f553922b096e2186b434
LNUAKX <lukmaannadaf@gmail.com>
2023-06-19 20:39:26 +0530

    modify

--- client/src/components/widjets/homeProfile.jsx
+++ client/src/components/widjets/homeProfile.jsx
@@ -44,7 +44,7 @@ const HomeProfile = () => {
 
           <div className="flex gap-4">
             <h1 className="text-xl mt-3 font-normal">
-              {user.friends.length} Connections l
+              {user.friends.length} Connections 
             </h1>
             <h1 className="text-xl mt-3 font-normal">{impressions} Views</h1>
           </div>
