9e5837d00b9d51cb3f8aadc945985d9cded303fc
LNUAKX <lukmaannadaf@gmail.com>
2023-07-10 20:49:36 +0530

    modify

---
+++ client/src/hooks/fectUser.hooks.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+axios.defaults.baseURL = "http://localhost:3000/";
+
+
+
+const useFecth=()=>{
+    const [userData,setUserdata]=useState({
+        isLoading:false,
+        Data:null,
+        serverError:null
+    })
+
+
+    useEffect(()=>{
+        const fecth=async ()=>{
+            try {
+                setUserdata((prev)=>({...prev,isLoading:true}))
+                const {data,status}=await axios.get('/api/users');
+                
+
+                if(status==200){
+                    setUserdata((prev)=>({...prev,isLoading:false}));
+                    setUserdata((prev)=>({...prev,Data:data}));
+                }else{
+                    setUserdata((prev)=>({...prev,serverError:"cannot get data",isLoading:false}));
+                }
+                
+                
+            } catch (error) {
+                setUserdata((prev)=>({...prev,serverError:error}))
+            }
+        }
+
+        fecth();
+    },[])
+
+
+    return [userData,setUserdata];
+
+
+
+  
+}
+
+
+export default useFecth;
