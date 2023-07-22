faa17973927152d54b44e36c8dd8c95c036f3591
LNUAKX <lukmaannadaf@gmail.com>
2023-07-22 18:14:16 +0530

    modify

--- client/src/components/widjets/homeProfile.jsx
+++ client/src/components/widjets/homeProfile.jsx
@@ -63,7 +63,7 @@ const HomeProfile = () => {
       </div>
       
       <div className="w-[100%] mr-0 flex flex-wrap justify-center text-white h-min p-5 max-sm:p-0">
-      <div className="flex justify-evenly gap-6 flex-wrap w-[95%] max-sm:w-[100%] max-sm:gap-2 h-[100%] max-sm:mt-10">
+      <div className="flex justify-items-start gap-6 flex-wrap w-[95%] max-sm:w-[100%] max-sm:gap-2 h-[100%] max-sm:mt-10">
       {feedPosts.map((item,index)=>{
         if(user._id===item.userId){
         return <Posts key={index} picturePath={item.picturePath} postUserId={item.userId} likes={item.likes} id={item._id}/>
