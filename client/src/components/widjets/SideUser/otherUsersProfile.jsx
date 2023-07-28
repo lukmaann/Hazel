a58398c6ecf6bd10db7927ab3f0b09a7e8dce57c
LNUAKX <lukmaannadaf@gmail.com>
2023-07-28 20:33:14 +0530

    modify

--- client/src/components/widjets/SideUser/otherUsersProfile.jsx
+++ client/src/components/widjets/SideUser/otherUsersProfile.jsx
@@ -9,12 +9,12 @@ const UsersProfile = (props) => {
   return (
     <div
       onClick={() => navigate("/user", { state: { id: name } })}
-      className=" w-[10%]  h-[90%] my-4 m-4 flex justify-center flex-col  px-3 items-center hover:cursor-pointer"
+      className=" w-min bg-white  h-[90%] my-4  flex  justify-center flex-col  px-3 items-center max-sm:z-30 max-sm:mt-10 max-sm:mb-0 hover:cursor-pointer"
     >
       <div className="h-[60px] w-[60px] p-[3px]   bg-gradient-to-r from-yellow-500 via-red-500 to-pink-500  justify-center items-center flex rounded-full mt-[20px] ">
         <img
           src={img||Avatar}
-          className=" border-white border-2 shadow-lg rounded-full h-[100%]   w-[100%]"
+          className=" border-white border-2  shadow-lg rounded-full h-[100%]   w-[100%]"
           alt=""
         />
       </div>
