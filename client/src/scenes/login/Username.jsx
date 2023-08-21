98b95f24ac6cbf1e1dbeb4db3e7a8a2ba8a861ff
LNUAKX <lukmaannadaf@gmail.com>
2023-08-21 10:22:35 +0530

    modify

--- client/src/scenes/login/Username.jsx
+++ client/src/scenes/login/Username.jsx
@@ -7,59 +7,58 @@ import { userValidate } from "../../helper/validate";
 import { useAuthStore, useUserStore } from "../../store/store";
 import { useNavigate } from "react-router-dom";
 import logo from "../../assets/logo.png";
-import Style from "./loginpages.module.css"
+import Style from "./loginpages.module.css";
 import useFecth from "../../hooks/fecth.hooks";
 import { useEffect, useState } from "react";
 import { LoadServer, loginUser } from "../../helper/helper";
-
+import Badge from "@mui/material/Badge";
 
 const Username = () => {
   const setUsername = useAuthStore((state) => state.setUsername);
-  const setUser=useUserStore((state)=>state.setUser)
+  const setUser = useUserStore((state) => state.setUser);
   const navigate = useNavigate();
 
-  const [isDisabled,setDisable]=useState(false)
- 
+  const [isDisabled, setDisable] = useState(false);
 
-  useEffect(()=>{
-    const serverloader=LoadServer();
+  useEffect(() => {
+    const serverloader = LoadServer();
 
-    setTimeout(()=>{
+    setTimeout(() => {
+      toast.promise(serverloader, {
+        loading: "Slow network detected ☹️",
+        success: "You can login now 😉",
+        error: "cant load app please try again",
+      });
+    }, 6000);
 
-      toast.promise(serverloader,{
-        loading:"Slow network detected ☹️",
-        success:"You can login now 😉",
-        error:"cant load app please try again"
-      })
-    
-    },6000)
+    toast.promise(serverloader, {
+      loading: "Booting Up the servers",
+      success: "Server started successfully",
+      error: "No network",
+    });
+  }, []);
 
-    toast.promise(serverloader,{
-      loading:"Booting Up the servers",
-      success:"Server started successfully",
-      error:"No network"
-    })
-  },[])
+  const [{ apiData }] = useFecth(`user/HELLOGUEST`);
+  const guestlogin = () => {
+    const loginPromise = loginUser({
+      username: "HELLOGUEST",
+      password: "helloguest",
+    });
 
-  const [{  apiData }] = useFecth(`user/HELLOGUEST`);
-  const guestlogin=()=>{
+    toast.promise(loginPromise, {
+      loading: "LOGGING IN AS GUEST",
+      success: "WELCOME GUEST",
+      error: "error",
+    });
 
-   const loginPromise= loginUser({username:"HELLOGUEST",password:"helloguest"})
+    loginPromise.then(async (res) => {
+      let { token } = res;
+      localStorage.setItem("token", token);
 
-   toast.promise(loginPromise,{
-    loading:"LOGGING IN AS GUEST",
-    success:"WELCOME GUEST",
-    error:"error"
-   })
-
-   loginPromise.then(async (res)=>{
-    let { token } = res;
-    localStorage.setItem("token", token);
-    
-    setUser(apiData);
-    navigate("/explore")
-   })
-  }
+      setUser(apiData);
+      navigate("/explore");
+    });
+  };
 
   const formik = useFormik({
     initialValues: {
@@ -70,33 +69,25 @@ const Username = () => {
     validateOnBlur: false,
     validateOnChange: false,
     onSubmit: async (value) => {
-     
-      if (value.username==="admin101"){
-        navigate('/admin')
-      }else{
+      if (value.username === "admin101") {
+        navigate("/admin");
+      } else {
         setUsername(value.username);
-        setDisable(true)
+        setDisable(true);
         navigate("/password");
-
       }
-     
     },
   });
 
-
-
-
   return (
-    <div >
+    <div>
       <Toaster
         toastOptions={{ style: { background: "#D2D2C0" } }}
         position="top-center"
         reverseOrder={false}
       ></Toaster>
       <div className="flex items-center justify-center h-screen ">
-        <div
-          className={`${Styles.glass} ${Style.logincontainer} `}
-        >
+        <div className={`${Styles.glass} ${Style.logincontainer} `}>
           <div className="title flex flex-col items-center">
             <img src={logo} className="text-3xl font-bold w-[200px]" />
             <span className="text-gray-500  text-l text-center py-3 w-2/3">
@@ -104,8 +95,7 @@ const Username = () => {
             </span>
           </div>
           <form className="py-1" onSubmit={formik.handleSubmit}>
-            <div className="profile flex justify-center py-4">
-            </div>
+            <div className="profile flex justify-center py-4"></div>
             <div className="textbox flex flex-col justify-center items-center gap-3">
               <input
                 type="text"
@@ -114,46 +104,49 @@ const Username = () => {
                 className={Styles.textbox}
                 autoComplete="OFF"
                 placeholder="Username"
-                
               />
-              <button className={`${Styles.btn} disabled:cursor-wait `} type="submit" disabled={isDisabled}>
+              <button
+                className={`${Styles.btn} disabled:cursor-wait `}
+                type="submit"
+                disabled={isDisabled}
+              >
                 Lets Go{" "}
               </button>
-             
             </div>
             <div className="text-center  mt-20 bg-black rounded-xl py-1 w-[70%] mx-auto text-white">
               <span>
                 Not a Member?
-                <Link to="/register" className=" text-red-500 px-2 hover:underline  font-bold">
+                <Link
+                  to="/register"
+                  className=" text-red-500 px-2 hover:underline  font-bold"
+                >
                   Register
                 </Link>
-                
               </span>
             </div>
           </form>
-          <div className="w-[100%] flex justify-center">
-         <h1 className="p-2 rounded-lg hover:cursor-pointer " onClick={guestlogin} >Login As Guest</h1>
-            
+          <div className="w-[100%] flex mt-4  justify-center">
+            <Badge color="warning" badgeContent="Try">
+              <h1
+                className="p-2 border border-black text-sm rounded-lg shadow-lg hover:cursor-pointer "
+                onClick={guestlogin}
+              >
+                Login As Guest
+              </h1>
+            </Badge>
           </div>
-             
-          
         </div>
         <div>
           <div className={`${Styles.home} relative flex max-sm:hidden`}>
-          
-      
             <button className=" absolute rounded-sm underlines  text-white px-2 bottom-2 right-2    ">
               {" "}
-             
               About Us ↗
-              
             </button>
           </div>
         </div>
       </div>
       <footer className="text-center text-sm text-gray-600 py-2">
-      ©{year} Lukn Developments  ||  All right reserved
-         
+        ©{year} Lukn Developments || All right reserved
       </footer>
     </div>
   );
