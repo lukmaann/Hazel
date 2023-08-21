863a6254aea9c1b83942097e808149357dfb5157
LNUAKX <lukmaannadaf@gmail.com>
2023-08-21 11:18:46 +0530

    modify

--- client/src/components/widjets/alldone.jsx
+++ client/src/components/widjets/alldone.jsx
@@ -1,7 +1,7 @@
 
 // import done from "../../assets/done.jpg"
 const AllDone=()=>{
-    return  <div className="h-[45vh]  bg-white shadow-2xl flex items-start w-[100%]">
+    return  <div className="h-[25vh]  bg-white shadow-2xl flex items-start w-[100%]">
     <div className="h-[90%] w-[100%]">
     <h1 className="text-center m-10  animate-pulse  text-2xl">All Done For Today</h1>
     {/* <img src={done} alt="" className=" " /> */}
