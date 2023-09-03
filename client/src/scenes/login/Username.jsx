1a46bb03a5f2cbea1bb40470c218c0db0ed41f63
LNUAKX <lukmaannadaf@gmail.com>
2023-09-03 13:32:25 +0530

    modify

--- client/src/scenes/login/Username.jsx
+++ client/src/scenes/login/Username.jsx
@@ -86,13 +86,13 @@ const Username = () => {
   });
 
   return (
-    <div className="container mx-auto bg-black">
+    <div className="container mx-auto ">
       <Toaster
         toastOptions={{ style: { background: "#D2D2C0" } }}
         position="top-center"
         reverseOrder={false}
       ></Toaster>
-      <div className="flex w-full items-center justify-center h-screen  ">
+      <div className="flex w-full items-center justify-center h-screen bg-black ">
         <div className={`${Styles.glass} ${Style.logincontainer} `}>
           <div className="title flex flex-col items-center">
             <img src={logo} className="text-3xl font-bold w-[200px]" />
